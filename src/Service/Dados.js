9200212fce7465e178ac68ed57e7a77b5480e42f
Rai Alcides <railayli2@hotmail.com>
2022-11-14 11:13:44 -0300

    pasta com o json

---
+++ src/Service/Dados.js
@@ -0,0 +1,40 @@
+const livros = [
+
+    {
+        id: 1,
+        titulo: 'Opreço de ser diferente',
+        descrição: 'Romero é um adolescente repleto de dúvidas e inseguranças em relação à sua orientação sexual. Depois de passar por uma situação traumática, descobre-se gay. Sem o apoio da família, sente-se sozinho e abandonado.',
+        Gênero: 'Ficção religiosa',
+        imagem: 'https://m.media-amazon.com/images/I/418mCsTyaxL.jpg'
+    },
+    {
+        id: 2,
+        titulo: 'Vida de droga',
+        descrição:'Dora sempre teve tudo o que havia de melhor até que seu pai perde o emprego e a família tem de baixar seu padrão de vida. A situação deixa a garota cada vez mais deprimida e revoltada, fazendo-a ir em direção às drogas e viver experiências pelas quais nunca esperou passar.',
+        Gênero: 'Romance',
+        imagem: 'https://m.media-amazon.com/images/I/41VXGRrWE2L.jpg',
+    },
+    {
+        id: 3,
+        titulo: 'Fallen',
+        descrição: 'Fallen é uma série de livros de romance e mistério sobre anjos escrita pela norte-americana Lauren Kate. A saga conta a história de Lucinda Price, uma garota que é mandada para um reformatório, tida como piromaníaca. Mas tudo muda na vida de Luce quando ela encontra Daniel Grigori, um jovem belo e misterioso, que acaba por se envolver com ela após salvar sua vida.',
+        Gênero: 'Ficção paranormal',
+        imagem: 'https://m.media-amazon.com/images/I/91mqmUaS9QL.jpg',
+    },
+    {
+        id: 4,
+        titulo: 'Sussurro',
+        descrição: 'Sussurro (em inglês: Hush, Hush) é um romance ficcional sobre anjos da escritora estadunidense Becca Fitzpatrick. A história é narrada da perspectiva de sua protagonista, Nora Grey, que vê sua vida em perigo quando começa a se envolver com o misterioso novo aluno de sua escola chamado Patch Cipriano, que mais tarde se revela como um anjo caído com quem possui uma obscura ligação.',
+        Gênero: 'Ficção / Romance / Mistério',
+        imagem: 'https://m.media-amazon.com/images/I/613McTRfJ4L.jpg',
+    },
+    {
+        id: 5,
+        titulo: 'Toda sua',
+        descrição:'Gideon Cross entrou na minha vida como um relâmpago na escuridão... Ele era inteligente, bem-sucedido, rico e muito lindo. Fiquei obcecada por ele como nunca tinha ficado por ninguém, por nada. Ansiava por seu toque como uma droga, mesmo sabendo que aquilo acabaria me destruindo. Eu tinha meus problemas, e ele fez com que viessem à tona muito facilmente. Gideon sabia.',
+        Gênero: 'Literatura erótica',
+        imagem: 'https://m.media-amazon.com/images/I/812LD2FD9yL.jpg',
+    }
+
+
+]
